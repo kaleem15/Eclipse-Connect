0eece95bcb6244445f2756c0eb48444867e4a1b7
kaleem15 <mohamed.kaleem@dedalus.eu>
2022-08-06 12:30:33 +0530

    connected git

---
+++ src/main/java/BasicJava/Project1/App.java
@@ -0,0 +1,13 @@
+package BasicJava.Project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
